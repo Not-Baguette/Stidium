ebc8fd27a748b7e4112ac55f4690e7080100f8e7
Baguette <94969176+Not-Baguette@users.noreply.github.com>
2022-12-25 17:21:34 +0700

    make it smaller

--- tests/generator.py
+++ tests/generator.py
@@ -6,7 +6,7 @@ def get_random_bytes(n):
     return os.urandom(n)
 
 
-for i in range(100000):
+for i in range(10000):
     with open(str(i+1) + ".txt", "wb") as file:
         file.write(get_random_bytes(1024))
         file.close()
