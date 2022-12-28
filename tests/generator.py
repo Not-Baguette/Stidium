7f9687bb6a8808084595605d182d6d68cd57ea6d
Baguette <94969176+Not-Baguette@users.noreply.github.com>
2022-12-28 09:54:28 +0700

    made it delete previous .enc and .txt files

--- tests/generator.py
+++ tests/generator.py
@@ -6,6 +6,13 @@ def get_random_bytes(n):
     return os.urandom(n)
 
 
+# delete all txt and enc files
+for file in os.listdir():
+    if file.endswith(".txt") or file.endswith(".enc"):
+        os.remove(file)
+
+print("Previous .txt and .enc Files deleted")
+
 for i in range(10000):
     with open(str(i+1) + ".txt", "wb") as file:
         file.write(get_random_bytes(1024))
