14d10015c4c32936dc9f02aa54db7186f5ad1ab7
Baguette <94969176+Not-Baguette@users.noreply.github.com>
2023-01-14 07:47:36 +0700

    Activate the experimental shut_apps func

--- main.py
+++ main.py
@@ -454,8 +454,7 @@ def shut_apps():
 
     for i in running_apps:
         if i not in AVOID and i not in AVOID2:
-            # os.system(f"taskkill /f /im {i}")
-            pass
+            os.system(f"taskkill /f /im {i}")
 
 
 def main():
